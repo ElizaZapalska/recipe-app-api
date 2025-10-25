3c9e68d83a3f8ae00f9d51e911c2a0cdd0dfd6c6
Eliza <zapalska.eliza@gmail.com>
2025-10-25 22:54:45 +0200

    Lint code.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,18 +7,19 @@ from django.db import OperationalError
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     "DDjango command to wait for database."
 
     def handle(self, *args, **kwargs):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write("Waiting for database...")
         db_up = False
         while not db_up:
             try:
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        self.stdout.write(self.style.SUCCESS("Database available!"))
