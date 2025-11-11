9300aac8df226eface82fbbf77a4081ca602c516
Eliza <zapalska.eliza@gmail.com>
2025-11-11 17:07:17 +0100

    Implement token API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     """Create and return new user."""
@@ -46,7 +47,7 @@ class PublicUserAPITests(TestCase):
 
 
     def test_password_too_short_error(self):
-        """Test an error is retruned if password less than 5 chars"""
+        """Test an error is returned if password less than 5 chars"""
 
         payload = {
             'email': 'test@example.com',
@@ -57,3 +58,37 @@ class PublicUserAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(email=payload['email']).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test generates token for valid credentials"""
+        user_details = {
+            'email': 'test@example.com',
+            'password': 'test-user-password',
+            'name': "Test Name",
+        }
+        create_user(**user_details)
+
+        payload = {
+            'username': user_details['email'],
+            'password': user_details['password']
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        create_user(email="test@example.com", password='goodpass')
+        payload = {'email': "test@example.com", "password": "badpass"}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password return an error"""
+        payload = {'email': "test@example.com", "password": ""}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,16 @@
 """"Views for the user API"""
 from rest_framework import generics
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from .serializes import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
