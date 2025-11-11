08bc6527ebf4cbca7c7f2a073b0c4dca26853434
Eliza <zapalska.eliza@gmail.com>
2025-11-11 18:36:00 +0100

    Implement manage user API.

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "drf_spectacular",
-    "user"
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -134,5 +134,5 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "core.User"
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
